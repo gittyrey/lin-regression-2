f61e1350e393d9eb675715cc267805345dc24af4
Reynolds Muchiri <wamuyurey@gmail.com>
2025-08-06 15:09:41 +0300

    updated code

--- student performance data set.py
+++ student performance data set.py
@@ -16,18 +16,39 @@ import keras
 students_training = pd.read_csv("https://raw.githubusercontent.com/guipsamora/pandas_exercises/master/04_Apply/Students_Alcohol_Consumption/student-mat.csv")
 
 ##Include a pandas code to tell us number of rows and columns (respectively)
-print(students_training.shape)
+print(f"The rows and columns used in the project are \n {students_training.shape}")
 
 ##to see columns mentioned in dataset
-print(students_training.columns)
+print(f"The columns available are\n {students_training.columns}")
 
 ##to find out datatypes of columns 
-print(students_training.dtypes)
+print(f"The datatypes of the columns are as follows \n {students_training.dtypes}")
 
 
 ##to filter desired columns
+student_performance = students_training.loc [:,("sex","studytime","traveltime","romantic","G1","G2","G3")]
 
-students_training_dataset = students_training.loc[:,('traveltime','studytime','failures','freetime','paid')]
+print (student_performance.columns)
 
-print(students_training_dataset.head(10))
+##statistics
+##to find the highest grade in G1 
+
+print(f"The best grade in the first term was {student_performance["G1"].max()}")
+
+##Best grade in G2
+print(f"The best grade in the second term was {student_performance["G2"].max()}")
+
+##Best grade in G3
+print(f"The best grade in the third term was {student_performance["G3"].max()}")
+
+##correlation matrix to identify the feature with the most impact
+
+correlation_matrix = student_performance.corr (numeric_only = True)
+print(correlation_matrix)
+
+
+##visualize correlation
+matrix_plot = px.scatter_matrix(student_performance, dimensions=["G1","G2","G3","traveltime","studytime"])
+
+matrix_plot.show()
 
